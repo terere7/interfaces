72ac97c791cce3cf388cd46f6ad01ba1712f6d44
tere.hsieh <tere.hsieh@gmail.com>
2020-09-25 20:06:59 -0300

    cambio estructura codigo

--- TP2/Entregable2/js/main.js
+++ TP2/Entregable2/js/main.js
@@ -1,28 +1,19 @@
+//CANVAS
 let canvas = document.querySelector('#canvas');
 let context = canvas.getContext('2d');
 let canvasWidth = canvas.width;
 let canvasHeight = canvas.height;
-const NUM_FIGURES = 10;
-const FIGURE_SIZE = 20;
+//FICHAS
+const FICHAS_SIZE = 20;
 const NUM_FICHAS = 21;
-let figures = []; // arreglo de figuras
-let lastClickedFigure = null;// ultima figura clickeada, por defecto no tengo ninguna
+let fichas = []; // arreglo de figuras
+let lastClickedFicha = null;// ultima figura clickeada, por defecto no tengo ninguna
 let isMouseDown = false;//esta clickeado
 
 
 
 //AGREGAR FIGURAS
 
-//Evento temporal para agregar figuras
-function addFigures() {
-    addFigure();
-    if (figures.length < 30) {
-        setTimeout(addFigures, 333);
-    }
-}
-// setTimeout(() => {
-//     addFigures();
-// }, 333);
 //Rectangulo
 function addRectangle() { // Agregar rectangulos al azar dentro del canvas
     //posicion random
@@ -30,20 +21,20 @@ function addRectangle() { // Agregar rectangulos al azar dentro del canvas
     let posY = Math.round(Math.random() * canvasHeight);
     let color = randomRGBA();// color random
     let rect = new Rect(posX, posY, 20, 20, color, context);
-    figures.push(rect);//agrega recangulos al arreglo
+    fichas.push(rect);//agrega recangulos al arreglo
 }
 //Circulo
 function addCircle(color,posX,posY) {
     //let color ='#600080';
-    let circle = new Circle(posX, posY, 20, color, context);
-    figures.push(circle);
+    let circle = new Circle(posX, posY, FICHAS_SIZE, color, context);
+    fichas.push(circle);
 }
 
 function drawFigures() {
     clearCanvas('#F8F8FF', canvas);
     //recorre el arreglo para dibujas las figuras
-    for (let i = 0; i < figures.length; i++) {
-        figures[i].draw();
+    for (let i = 0; i < fichas.length; i++) {
+        fichas[i].draw();
     }
 }
 
@@ -63,8 +54,8 @@ function clearCanvas(color, canvas) {
 }
 // OBTENER FIGURA CLICKEADA
 function findClickedFigure(x, y) {
-    for (let index = 0; index < figures.length; index++) {
-        const element = figures[index];
+    for (let index = 0; index < fichas.length; index++) {
+        const element = fichas[index];
         if (element.isPointInside(x, y)) {
             return element;
         }
@@ -77,24 +68,24 @@ function onMouseDown(event) {
     isMouseDown = true;
 
     // Se limpia la propiedad highlighted de la ultima figura clickeada
-    if (lastClickedFigure != null) {
-        lastClickedFigure.setHighlighted(false);
-        lastClickedFigure = null;
+    if (lastClickedFicha != null) {
+        lastClickedFicha.setHighlighted(false);
+        lastClickedFicha = null;
     }
 
     // Buscar si hay una nueva figura clickeada
     let clickedFigure = findClickedFigure(event.layerX, event.layerY);
     if (clickedFigure != null) {
         clickedFigure.setHighlighted(true);
-        lastClickedFigure = clickedFigure;
+        lastClickedFicha = clickedFigure;
     }
 
     drawFigures();
 }
 
 function onMouseMoved(event) {
-    if (isMouseDown && lastClickedFigure != null) {
-        lastClickedFigure.setPosition(event.layerX, event.layerY);
+    if (isMouseDown && lastClickedFicha != null) {
+        lastClickedFicha.setPosition(event.layerX, event.layerY);
         drawFigures();
     }
 }
@@ -117,14 +108,12 @@ function crearFichas() {
         
     }
     drawFigures();
-
     // Inicializar listeners de eventos de mouse en el canvas
     canvas.addEventListener('mousedown', onMouseDown, false);
     canvas.addEventListener('mouseup', onMouseUp, false);
     canvas.addEventListener('mousemove', onMouseMoved, false);
 
 }
-//#endr
 crearFichas();
 
 
