1a91eac97f35dac631a9ebc64ad6ec06dee5d0d4
terehsieh <42392544+terehsieh@users.noreply.github.com>
2020-10-20 19:04:04 -0300

    form agregado de alert

--- TP3/js/form.js
+++ TP3/js/form.js
@@ -5,16 +5,26 @@ window.addEventListener('load', () => {
     function checkForm() {
         let username = document.querySelector("#username").value;
         let comment = document.querySelector("#comment").value;
-        if (username === "" || comment === "") {
-            console.log(username);
-            console.log(comment)
-            alert("You must complete all the inputs");
+        if (username === "") {
+            //  alert("You must complete all the inputs");
+            document.querySelector(".alert").classList.remove('hide');
+            document.querySelector(".alert").classList.add('show');
         } else {
+            document.querySelector(".alert").classList.remove('show');
+            document.querySelector(".alert").classList.add('hide');
+        }
+        if (comment === "") {
+            //  alert("You must complete all the inputs");
+            document.querySelector(".alertC").classList.remove('hide');
+            document.querySelector(".alertC").classList.add('show');
+        } else {
+            document.querySelector(".alertC").classList.remove('show');
+            document.querySelector(".alertC").classList.add('hide');
+        }
+        if (username !== "" && comment !== "") {
             let form = document.querySelector(".form");
-
             form.innerHTML = "";
             form.innerHTML = '<div class="msg" ><h2>Your comment was succesfully sent. </h2><h3>' + username + ', thank you for your feedback!</h3><button type="submit"><a href="form.html">Back</a></button></div>';
-
         }
     }
 });
